19ae7408ece38ed7ab540ab70b066bee35468b49
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-03 16:56:34 -0300

    feat(ocr_base.py): making execution time

--- ocr_base.py
+++ ocr_base.py
@@ -1,6 +1,8 @@
 import pytesseract as ocr
-
 from PIL import Image
+from time import time
 
-phrase = ocr.image_to_string(Image.open('phrase.jpg'), lang='por')
+start_time = time()
+phrase = ocr.image_to_string(Image.open('./images/arroz.jpg'), lang='por')
 print(phrase)
+print("Execution Time:", time() - start_time)
