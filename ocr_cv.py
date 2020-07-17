1c19e270ccaf28cf26e96344bc24b4c721474b22
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-17 14:06:16 -0300

    indentação

--- ocr_cv.py
+++ ocr_cv.py
@@ -9,26 +9,28 @@ import numpy as np
 
 start_time = time()
 # tipando a leitura para os canais de ordem RGB
-imagem = Image.open('./images/tabela.png').convert('RGB')
+imagem = Image.open('./images/a1.jpg').convert('RGB')
 
 # convertendo em um array editável de numpy[x, y, CANALS]
-npimagem = np.asarray(imagem).astype(np.uint8)  
+npimagem = np.asarray(imagem).astype(np.uint8)
 
 # diminuição dos ruidos antes da binarização
-npimagem[:, :, 0] = 0 # zerando o canal R (RED)
-npimagem[:, :, 2] = 0 # zerando o canal B (BLUE)
+npimagem[:, :, 0] = 0  # zerando o canal R (RED)
+npimagem[:, :, 2] = 0  # zerando o canal B (BLUE)
 
 # atribuição em escala de cinza
-im = cv2.cvtColor(npimagem, cv2.COLOR_RGB2GRAY) 
+im = cv2.cvtColor(npimagem, cv2.COLOR_RGB2GRAY)
 
 # aplicação da truncagem binária para a intensidade
 # pixels de intensidade de cor abaixo de 127 serão convertidos para 0 (PRETO)
 # pixels de intensidade de cor acima de 127 serão convertidos para 255 (BRANCO)
 # A atrubição do THRESH_OTSU incrementa uma análise inteligente dos nivels de truncagem
-ret, thresh = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) 
+ret, thresh = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
 # reconvertendo o retorno do threshold em um objeto do tipo PIL.Image
-binimagem = Image.fromarray(thresh) 
+binimagem = Image.fromarray(thresh)
+
+end_time = time()
 
 plt.axis("off")
 plt.imshow(cv2.cvtColor(np.float32(binimagem), cv2.COLOR_BGR2RGB))
@@ -39,5 +41,5 @@ plt.show()
 phrase = ocr.image_to_string(binimagem, lang='por')
 
 # impressão do resultado
-print(phrase) 
-print("Execution Time:", time() - start_time)
+print(phrase)
+print("Execution Time:", end_time - start_time)
