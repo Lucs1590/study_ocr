02bd248bcc4b872c06aa1135c6c3c475091047a2
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-19 17:21:15 -0300

    "feat(table _ocr.py): erode, dilate and alpha remotion"

--- table_ocr.py
+++ table_ocr.py
@@ -5,7 +5,7 @@ from PIL import Image
 from time import time
 
 
-def detect_edges(image):
+""" def detect_edges(image):
     edges = cv2.Canny(image, 50, 150, apertureSize=3)
     cv2.imwrite('images/canny1.jpg', edges)
     minLineLength = 100
@@ -18,7 +18,7 @@ def detect_edges(image):
                                                            [0][2], lines[i][0][3]), (0, 0, 255), 1, cv2.LINE_AA)
         cv2.imwrite('houghlines.jpg', image)
         cv2.imshow('img', image)
-        cv2.waitKey(0)
+        cv2.waitKey(0) """
 
 
 def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -53,9 +53,25 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     return resized
 
 
+def erode(img, kernel_size):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    return cv2.erode(img, kernel, iterations=3)
+
+
+def dilate(img, kernel_size):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    return cv2.dilate(img, kernel, iterations=1)
+
+
 # read image
-image = cv2.imread('images/tabela.png')
+image = cv2.imread('images/t1.png')
+# removing alpha chanel
+image = image[:, :, :3]
 # increase image 4 times
 image = image_resize(image, height=image.shape[0]*4)
-cv2.imshow('output', image)
+# erode
+image = erode(image, 3)
+# dilate
+image = dilate(image, 5)
+cv2.imwrite('output1.png', image)
 cv2.waitKey(0)
