96e3d91209639d9330589c03cb0855acfc0221e4
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-20 15:21:06 -0300

    "feat(table_ocr.py): remove lines method"

--- table_ocr.py
+++ table_ocr.py
@@ -5,7 +5,7 @@ from PIL import Image
 from time import time
 
 
-""" def detect_edges(image):
+def detect_edges(image):
     edges = cv2.Canny(image, 50, 150, apertureSize=3)
     cv2.imwrite('images/canny1.jpg', edges)
     minLineLength = 100
@@ -18,7 +18,7 @@ from time import time
                                                            [0][2], lines[i][0][3]), (0, 0, 255), 1, cv2.LINE_AA)
         cv2.imwrite('houghlines.jpg', image)
         cv2.imshow('img', image)
-        cv2.waitKey(0) """
+        cv2.waitKey(0)
 
 
 def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -63,15 +63,56 @@ def dilate(img, kernel_size):
     return cv2.dilate(img, kernel, iterations=1)
 
 
+def remove_lines(image):
+    # Color changes
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh_val, thresh = cv2.threshold(
+        gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    # Making kernels
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 1))
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 25))
+
+    # Detect lines
+    detected_h_lines = cv2.morphologyEx(
+        thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+    detected_v_lines = cv2.morphologyEx(
+        thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
+
+    # Remove lines
+    h_cnts = cv2.findContours(
+        detected_h_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    h_cnts = h_cnts[0] if len(h_cnts) == 2 else h_cnts[1]
+    for c in h_cnts:
+        cv2.drawContours(image, [c], -1, (255, 255, 255), 2)
+
+    v_cnts = cv2.findContours(
+        detected_v_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    v_cnts = v_cnts[0] if len(v_cnts) == 2 else v_cnts[1]
+    for c in v_cnts:
+        cv2.drawContours(image, [c], -1, (255, 255, 255), 2)
+
+    # Repair image
+    repair_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+    result = 255 - cv2.morphologyEx(255 - image,
+                                    cv2.MORPH_CLOSE, repair_kernel, iterations=1)
+
+    cv2.imwrite('tests/thresh1.png', thresh)
+    # cv2.imwrite('tests/detected_lines0.png', detected_h_lines)
+    # cv2.imwrite('tests/detected_lines1.png', detected_v_lines)
+    cv2.imwrite('tests/image.png', image)
+    # cv2.imwrite('tests/result1.png', result)
+    cv2.waitKey()
+
+
 # read image
-image = cv2.imread('images/t1.png')
+image = cv2.imread('images/tabela.png')
 # removing alpha chanel
 image = image[:, :, :3]
+# remove lines
+remove_lines(image)
 # increase image 4 times
 image = image_resize(image, height=image.shape[0]*4)
-# erode
-image = erode(image, 3)
-# dilate
-image = dilate(image, 5)
-cv2.imwrite('output1.png', image)
+
+cv2.imwrite('tests/output1.png', image)
 cv2.waitKey(0)
