3283581d37e43094e15963beef07a204520545d8
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-21 14:39:30 -0300

    "feat(table_ocr.py): unsharp mask into image"

--- table_ocr.py
+++ table_ocr.py
@@ -152,6 +152,20 @@ def histo_optimization(image, alpha=1.5, beta=0):
     return adjusted
 
 
+def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+    """Return a sharpened version of the image, using an unsharp mask."""
+    # https://homepages.inf.ed.ac.uk/rbf/HIPR2/unsharp.htm
+    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+    sharpened = float(amount + 1) * image - float(amount) * blurred
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(image - blurred) < threshold
+        np.copyto(sharpened, image, where=low_contrast_mask)
+    return sharpened
+
+
 # read image
 image = cv2.imread('images/2tabela.png')
 # removing alpha chanel
@@ -168,6 +182,8 @@ image = image_resize(image, height=image.shape[0]*4)
 image = open_close(cv2.MORPH_CLOSE)
 # histogram and contrast optimization
 image = histo_optimization(image, 1, 0.5)
+# improve sharp
+image = unsharp_mask(image, (3, 3), 0.5, 1.5, 0)
 
 
 cv2.imwrite('tests/output1.png', image)
