ebffb1a697e76d30f5b770d092960eae9969734b
Lucas Brito <lucasbsilva29@gmail.com>
2020-07-21 10:52:00 -0300

    "style(table_ocr.py): identation"

--- table_ocr.py
+++ table_ocr.py
@@ -119,9 +119,9 @@ def open_close(method, kernel=2):
 def clahe(image):
     lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
     l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     cl = clahe.apply(l)
-    limg = cv2.merge((cl,a,b))
+    limg = cv2.merge((cl, a, b))
     final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
     cv2.imwrite('tests/clahe1.png', final)
 
